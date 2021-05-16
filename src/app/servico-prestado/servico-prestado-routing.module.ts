a9d19e9d8c710c452f42e65c827f8ec8b9a5c31d
Juliano Ramos <julianovsr@gmail.com>
2021-05-15 21:43:22 -0300

    Criação de Componente Layout Refatoração de URL's

--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
 import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-prestado-lista.component';
+import { LayoutComponent } from '../layout/layout.component'
 
 const routes: Routes = [
-  {path: 'servico-prestado-form', component: ServicoPrestadoFormComponent},
-  {path: 'servico-prestado-lista', component: ServicoPrestadoListaComponent}
+  
+  {path: 'servicos', component : LayoutComponent, children : [
+    {path: 'form', component: ServicoPrestadoFormComponent},
+    {path: 'lista', component: ServicoPrestadoListaComponent},
+    {path: '', redirectTo : '/servicos/lista', pathMatch : 'full'},
+  ]},
+
+  
+  
 ];
 
 @NgModule({
